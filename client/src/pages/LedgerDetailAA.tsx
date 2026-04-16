b8b22576fbe93002bad683f1956b02f082cae95f
runyi329 <runyi329@github.com>
2026-04-16 06:43:43 -0400

    fix: 数字B图标放大至113%，完全遮住容器边缘

--- client/src/pages/LedgerDetailAA.tsx
+++ client/src/pages/LedgerDetailAA.tsx
@@ -720,7 +720,7 @@ export default function LedgerDetailAA({
                   <img
                     src="https://d2xsxph8kpxj0f.cloudfront.net/310519663279996243/ivirPqo3t2YCdg32vqitTK/btc_icon_6d044b9b.png"
                     alt="数字B"
-                    style={{ width: '108%', height: '108%', objectFit: 'cover', position: 'absolute', top: '50%', left: '50%', transform: 'translate(-50%, -50%)' }}
+                    style={{ width: '113%', height: '113%', objectFit: 'cover', position: 'absolute', top: '50%', left: '50%', transform: 'translate(-50%, -50%)' }}
                   />
                 </div>
               )}
