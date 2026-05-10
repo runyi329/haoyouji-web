af32152543454a29445895c26d5ef1ca7ea013f8
runyi329 <runyihongkong@gmail.com>
2026-05-10 22:30:44 +0800

    style: 更新商标为彻底透明背景版本v3

--- client/src/pages/PetPlatform.tsx
+++ client/src/pages/PetPlatform.tsx
@@ -499,7 +499,7 @@ export default function PetPlatform() {
             </button>
             {/* HaGeeMe 商标图片替换标题文字 */}
             <img
-              src="https://haoyouji-images-1396946788.cos.ap-shanghai.myqcloud.com/pet-platform/hageme-logo-transparent.png?v=2"
+              src="https://haoyouji-images-1396946788.cos.ap-shanghai.myqcloud.com/pet-platform/hageme-logo-transparent.png?v=3"
               alt="HaGeeMe"
               className="h-20 w-auto object-contain"
               style={{ filter: 'drop-shadow(0 1px 4px rgba(0,0,0,0.3))' }}
